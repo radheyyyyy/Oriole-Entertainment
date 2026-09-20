23d4eb6962d17b575113aec0be54f152355b91fe
radheyyyyy <jangraradhey5@gmail.com>
2026-09-20 23:21:00 +0530

    feat: add ArtistList component and bassi image asset

--- src/components/ArtistList.jsx
+++ src/components/ArtistList.jsx
@@ -223,7 +223,7 @@ const ArtistList = () => {
       {
         id: "Bassi Sponsorship.Deck (2)_compressed.pdf",
         name: "Anubhav Singh Bassi",
-        image: "bassi.jpg",
+        image: "bassi.png",
         description: "Master of relatable anecdotes and storytelling.",
         shows: [
           "Sep 11, 2026 - Ahmedabad",
